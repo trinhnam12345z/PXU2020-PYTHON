f29d7bf4f0847b615b0459fabce1d7fa17b21946
trinhnam12345z <trinhnam123456789z@gmail.com>
2021-01-12 15:11:29 +0700

    Bai 10

--- Bai-10.py
+++ Bai-10.py
@@ -1,29 +1,49 @@
 import numpy as np
 import pandas as pd
-import matplotlib as plt
-
-# _A = [[1, 2, 3], [4, 5, 6]]
-# _B = [[6, 5, 4], [3, 2, 1]]
-# A = np.array(_A)
-# B = np.array(_B)
-# print('A = ')
-# print(A)
-# print('B = ')
-# print(B)
-# print('A + B =')
-# print(A + B)
-# print('A * B =')
-# print(A * B)
-# print('Chuyển vị A :')
-# print(A.transpose())
-# print('Chuyển vị B :')
-# print(B.transpose())
-#
+import matplotlib.pyplot as plt
+# 1 .
+print("================================================")
+print("1.")
+_A = [[1, 2, 3], [4, 5, 6]]
+_B = [[6, 5, 4], [3, 2, 1]]
+A = np.array(_A)
+B = np.array(_B)
+print('A = ')
+print(A)
+print('B = ')
+print(B)
+print('A + B =')
+print(A + B)
+print('A * B =')
+print(A * B)
+print('Chuyển vị A :')
+print(A.transpose())
+print('Chuyển vị B :')
+print(B.transpose())
+print("================================================")
 # s = pd.Series([1, 3, 5, np.nan, 6, 8])
 # print(s)
+# 2 .
+print("2.")
+#đọc từ máy
+a = pd.read_excel("E:\Python\Book1.xlsx")
+print(a)
+print("================================================")
+# đọc từ internet
+url = r'https://www1.ncdc.noaa.gov/pub/data/cdo/samples/PRECIP_HLY_sample_csv.csv'
+df = pd.read_csv(url)
+print(df)
+print("================================================")
+print("3.")
 
-# a = pd.read_excel("E:\Python\Book1.xlsx")
-# print(a)
+x = [2, 4, 6]
+y = [4, 3, 5]
+
+#Vẻ biểu đồ
+plt.plot(x, y)
+plt.boxplot(x, y)
+plt.bar(x,y)
+plt.show()
 
 
 
